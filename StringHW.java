2a09883672979f2bb571b0875a99c89055e0f00c
sulida <126574768+sulida@users.noreply.github.com>
2023-03-15 17:53:26 +0200

    String Java Home Work

---
+++ StringHW.java
@@ -0,0 +1,13 @@
+import java.util.SortedMap;
+
+public class StringHW {
+    public static void main(String[] args) {
+        String a = "Hello, Java! It's a Java Basic course";
+
+        System.out.println("Длина строки = " + a.length());
+        System.out.println("Cимвол 11 = " + a.charAt(11));
+        System.out.println(a.substring(13,24));
+        System.out.println(a.substring(20,37));
+
+    }
+}
